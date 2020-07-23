08e805bc939c7ddda29aee4a06af2d2dc95d93ab
AoTxLand <aotxland@gmail.com>
2020-07-23 11:17:44 +0800

    使用renren-generator生成基本功能

--- product/src/test/java/com/aotxland/mall/product/ProductApplicationTests.java
+++ product/src/test/java/com/aotxland/mall/product/ProductApplicationTests.java
@@ -1,13 +1,23 @@
 package com.aotxland.mall.product;
 
+import com.aotxland.mall.product.entity.BrandEntity;
+import com.aotxland.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("xiaomi");
+        brandEntity.setName("mi");
+        brandService.save(brandEntity);
     }
 
 }
